049153ec251751bf0403ace88b17f7bd8934cf14
Ariel Claros <exdoog@gmail.com>
2019-09-25 17:08:34 -0400

    set  y get

--- main.ts
+++ main.ts
@@ -1,40 +1,78 @@
 export class Bar {
-    nombre: string;     //atributo
-    capacidad: number;
+    private _nombre: string;     //atributo
+    private _capacidad: number;
     constructor(nom: string, cap: number) {
 
-        this.nombre = nom;
-        this.capacidad = cap;
+        this._nombre = nom;
+        this._capacidad = cap;
     }
+    public setNombre(nom: string):void{
+        this._nombre=nom;
+        
+    }
+   public getNombre(): string{
+        return this._nombre;
+   }
+   public setCapacidad(cap:number):void{
 
+    this._capacidad=cap;
 
-
-
+   }
+   public getcapaciad(): number{
+       return this._capacidad;
+   }
 }
 export class BarMan {
 
-    nombre: string;
-    apellido: string;
-    constructor (nom:string,ap:string){
-        this.nombre=nom;
-        this.apellido=ap;
+    private nombre: string;
+    private apellido: string;
+    constructor(nom: string, ap: string) {
+        this.nombre = nom;
+        this.apellido = ap;
     }
+    public getNombre(): string {
+        return this.nombre;
+    }
+  public setNombre(nom: string):void{
 
-
-
+    this.nombre=nom;
+  }
+  public  getApellido(): string{
+      return this.apellido;
+  }
+  public setApellido(ap:string ){
+      this.apellido=ap;
+  }
 }
 
 export class Trago {
-    nombre: string;
-    color: string;
-    cantidad: number;
-    constructor (nom:string,col:string,cant:number){
+    private nombre: string;
+    private color: string;
+    private cantidad: number;
+    constructor(nom: string, col: string, cant: number) {
 
-    this.nombre=nom;
-    this.color=col;
-    this.cantidad=cant;
+        this.nombre = nom;
+        this.color = col;
+        this.cantidad = cant;
     }
-
+    public setNombre(nom:string){
+        this.nombre=nom;
+    }
+    public getNombre():string{
+        return this.nombre;
+    }
+    public setColor(col:string){
+        this.color=col;
+    }
+    public getColor():string{
+        return this.color;
+    }
+    public setCantidad(cant:number){
+        this.cantidad=cant;
+    }
+    public getCcantidad():number{
+    return this.cantidad;
+    }
 }
 
 /******************************************************************* */
@@ -43,9 +81,11 @@ export class Trago {
 
 let bar1: Bar = new Bar('nueva era ', 50);
 let bar2: Bar = new Bar('el patio', 200);
-let barman1:BarMan=new BarMan('el topo','caca');
-let trago1:Trago= new Trago('gaipirina','morado',3);
+let barman1: BarMan = new BarMan('bryan', 'poca');
+let trago1: Trago = new Trago('gaipirina', 'morado', 3);
 console.log(bar1);
 console.log(bar2);
 console.log(barman1);
 console.log(trago1);
+console.log(bar1.getNombre());
+console.log(bar1.setCapacidad(60));
